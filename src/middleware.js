ff004cde57c1581cabea6d521921f10899d16cfb
Arunesh Akash <akashkr099@gmail.com>
2024-10-16 02:41:54 +0530

    Add credentials to remove cors error

--- src/middleware.js
+++ src/middleware.js
@@ -2,37 +2,39 @@ import { NextResponse } from 'next/server';
 
 export function middleware(request) {
     const authToken = request.cookies.get('authToken');
-    const userNotLoggedIn = request.nextUrl.pathname === "/login";
-
+    const isLoginPage = request.nextUrl.pathname === "/login";
+    const isProtectedRoute = ["/cart", "/shop", "/place-order"].includes(request.nextUrl.pathname);
 
     const response = NextResponse.next();
 
 
-    if (request.nextUrl.pathname.startsWith('/*')) {
+    if (request.method === 'OPTIONS') {
         response.headers.set('Access-Control-Allow-Credentials', 'true');
         response.headers.set('Access-Control-Allow-Origin', '*');
         response.headers.set('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
         response.headers.set('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+        return response;
     }
 
-    console.log('middleware executed');
-    console.log(authToken);
+    console.log('Auth Token:', authToken);
 
-    if (userNotLoggedIn && authToken) {
+
+    if (isLoginPage && authToken) {
         return NextResponse.redirect(new URL('/shop', request.url));
-    } else {
-        if (!authToken) {
-            return NextResponse.redirect(new URL('/login', request.url));
-        }
+    }
+
+
+    if (!authToken && isProtectedRoute) {
+        return NextResponse.redirect(new URL('/login', request.url));
     }
 
     return response;
 }
+
 export const config = {
     matcher: [
         "/cart",
         "/shop",
         "/place-order",
-
-    ]
+    ],
 };
